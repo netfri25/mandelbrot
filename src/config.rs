d563c45dd9b2af9b93f7c199f1f6627219d34b06
Netanel Friedman <netanel.friedman6@gmail.com>
2026-08-30 14:48:56 +0300

    Reduce weird compile time macros in config

--- src/config.rs
+++ src/config.rs
@@ -38,40 +38,6 @@ pub struct Config {
     pub iterations: u32,
 }
 
-macro_rules! create_macroquad_program_body {
-    ($producer:expr, $resolution:expr) => {{
-        let mut producer = $producer;
-        let mut renderer = $crate::renderer::macroquad::MacroquadRenderer::new(
-            1.,
-            Default::default(),
-            $resolution,
-        );
-
-        let program = async move {
-            loop {
-                ::macroquad::prelude::clear_background(::macroquad::prelude::BLACK);
-                $crate::renderer::Renderer::render(&mut renderer, &mut producer);
-                ::macroquad::prelude::next_frame().await
-            }
-        };
-
-        Box::pin(program)
-    }};
-}
-
-fn make_threaded<F, P>(threads: u32, mut make_producer: F) -> Box<dyn Producer + Send>
-where
-    ThreadedProducer<F>: Producer,
-    F: FnMut() -> P + Send + 'static,
-    P: Producer + Send + 'static,
-{
-    if threads == 1 {
-        Box::new(make_producer())
-    } else {
-        Box::new(ThreadedProducer::new(threads as usize, make_producer))
-    }
-}
-
 impl Config {
     pub fn create_macroquad_program(self) -> Pin<Box<dyn Future<Output = ()>>> {
         rayon::ThreadPoolBuilder::new()
@@ -80,46 +46,22 @@ impl Config {
             .unwrap();
 
         let resolution = self.resolution.0;
+        let mut producer = self.create_producer();
+        let mut renderer = crate::renderer::macroquad::MacroquadRenderer::new(
+            1.,
+            Default::default(),
+            resolution,
+        );
 
-        match self.number_type {
-            NumberType::F64 => {
-                let producer = make_threaded(self.threads, move || match self.simd {
-                    0 => self.create_naive_producer::<f64>(),
-                    lanes => self.create_simd_producer::<f64>(lanes),
-                });
-                create_macroquad_program_body!(producer, resolution)
+        let program = async move {
+            loop {
+                macroquad::prelude::clear_background(::macroquad::prelude::BLACK);
+                crate::renderer::Renderer::render(&mut renderer, producer.as_mut());
+                macroquad::prelude::next_frame().await
             }
+        };
 
-            NumberType::F32 => {
-                let producer = make_threaded(self.threads, move || match self.simd {
-                    0 => self.create_naive_producer::<f32>(),
-                    lanes => self.create_simd_producer::<f32>(lanes),
-                });
-                create_macroquad_program_body!(producer, resolution)
-            }
-
-            NumberType::FastF64 => {
-                let producer = make_threaded(self.threads, move || {
-                    self.create_naive_producer::<FastF64>()
-                });
-                create_macroquad_program_body!(producer, resolution)
-            }
-
-            NumberType::FastF32 => {
-                let producer = make_threaded(self.threads, move || {
-                    self.create_naive_producer::<FastF32>()
-                });
-                create_macroquad_program_body!(producer, resolution)
-            }
-
-            NumberType::Posit => {
-                let producer = make_threaded(self.threads, move || {
-                    self.create_naive_producer::<fast_posit::p64>()
-                });
-
-                create_macroquad_program_body!(producer, resolution)
-            }
-        }
+        Box::pin(program)
     }
 
     pub fn window_config(&self) -> macroquad::window::Conf {
@@ -136,17 +78,52 @@ impl Config {
         }
     }
 
+    fn create_producer(&self) -> Box<dyn Producer + Send> {
+        match self.number_type {
+            NumberType::F64 => self.create_simd_or_naive_producer::<f64>(self.simd),
+            NumberType::F32 => self.create_simd_or_naive_producer::<f32>(self.simd),
+            NumberType::FastF64 => self.create_naive_producer::<FastF64>(),
+            NumberType::FastF32 => self.create_naive_producer::<FastF32>(),
+            NumberType::Posit => self.create_naive_producer::<fast_posit::p64>(),
+        }
+    }
+
+    fn make_threaded<F, P>(&self, mut make_producer: F) -> Box<dyn Producer + Send>
+    where
+        ThreadedProducer<F>: Producer,
+        F: FnMut() -> P + Send + 'static,
+        P: Producer + Send + 'static,
+    {
+        if self.threads == 1 {
+            Box::new(make_producer())
+        } else {
+            Box::new(ThreadedProducer::new(self.threads as usize, make_producer))
+        }
+    }
+
+    fn create_simd_producer<T, const LANES: usize>(&self) -> Box<dyn Producer + Send>
+    where
+        T: Send + 'static,
+        crate::producer::simd::SimdProducer<T, LANES>: crate::producer::Producer,
+    {
+        let iterations = self.iterations;
+        self.make_threaded(move || crate::producer::simd::SimdProducer::<T, LANES>::new(iterations))
+    }
+
     fn create_naive_producer<T>(&self) -> Box<dyn Producer + Send>
     where
-        crate::producer::naive::NaiveProducer<T>: Producer + Send + 'static,
+        T: Send + 'static,
+        crate::producer::naive::NaiveProducer<T>: crate::producer::Producer,
     {
-        Box::new(crate::producer::naive::NaiveProducer::new(self.iterations))
+        let iterations = self.iterations;
+        self.make_threaded(move || crate::producer::naive::NaiveProducer::<T>::new(iterations))
     }
 
     // NOTE: this can probably be done with code generation, but a using `g<c-a>` in vim is enough
-    fn create_simd_producer<T>(&self, lanes: u8) -> Box<dyn Producer + Send>
+    fn create_simd_or_naive_producer<T>(&self, lanes: u8) -> Box<dyn Producer + Send>
     where
         T: Send + 'static,
+        crate::producer::naive::NaiveProducer<T>: Producer + Send + 'static,
         crate::producer::simd::SimdProducer<T, 1>: crate::producer::Producer,
         crate::producer::simd::SimdProducer<T, 2>: crate::producer::Producer,
         crate::producer::simd::SimdProducer<T, 3>: crate::producer::Producer,
@@ -213,199 +190,71 @@ impl Config {
         crate::producer::simd::SimdProducer<T, 64>: crate::producer::Producer,
     {
         match lanes {
-            1 => Box::new(crate::producer::simd::SimdProducer::<T, 1>::new(
-                self.iterations,
-            )),
-            2 => Box::new(crate::producer::simd::SimdProducer::<T, 2>::new(
-                self.iterations,
-            )),
-            3 => Box::new(crate::producer::simd::SimdProducer::<T, 3>::new(
-                self.iterations,
-            )),
-            4 => Box::new(crate::producer::simd::SimdProducer::<T, 4>::new(
-                self.iterations,
-            )),
-            5 => Box::new(crate::producer::simd::SimdProducer::<T, 5>::new(
-                self.iterations,
-            )),
-            6 => Box::new(crate::producer::simd::SimdProducer::<T, 6>::new(
-                self.iterations,
-            )),
-            7 => Box::new(crate::producer::simd::SimdProducer::<T, 7>::new(
-                self.iterations,
-            )),
-            8 => Box::new(crate::producer::simd::SimdProducer::<T, 8>::new(
-                self.iterations,
-            )),
-            9 => Box::new(crate::producer::simd::SimdProducer::<T, 9>::new(
-                self.iterations,
-            )),
-            10 => Box::new(crate::producer::simd::SimdProducer::<T, 10>::new(
-                self.iterations,
-            )),
-            11 => Box::new(crate::producer::simd::SimdProducer::<T, 11>::new(
-                self.iterations,
-            )),
-            12 => Box::new(crate::producer::simd::SimdProducer::<T, 12>::new(
-                self.iterations,
-            )),
-            13 => Box::new(crate::producer::simd::SimdProducer::<T, 13>::new(
-                self.iterations,
-            )),
-            14 => Box::new(crate::producer::simd::SimdProducer::<T, 14>::new(
-                self.iterations,
-            )),
-            15 => Box::new(crate::producer::simd::SimdProducer::<T, 15>::new(
-                self.iterations,
-            )),
-            16 => Box::new(crate::producer::simd::SimdProducer::<T, 16>::new(
-                self.iterations,
-            )),
-            17 => Box::new(crate::producer::simd::SimdProducer::<T, 17>::new(
-                self.iterations,
-            )),
-            18 => Box::new(crate::producer::simd::SimdProducer::<T, 18>::new(
-                self.iterations,
-            )),
-            19 => Box::new(crate::producer::simd::SimdProducer::<T, 19>::new(
-                self.iterations,
-            )),
-            20 => Box::new(crate::producer::simd::SimdProducer::<T, 20>::new(
-                self.iterations,
-            )),
-            21 => Box::new(crate::producer::simd::SimdProducer::<T, 21>::new(
-                self.iterations,
-            )),
-            22 => Box::new(crate::producer::simd::SimdProducer::<T, 22>::new(
-                self.iterations,
-            )),
-            23 => Box::new(crate::producer::simd::SimdProducer::<T, 23>::new(
-                self.iterations,
-            )),
-            24 => Box::new(crate::producer::simd::SimdProducer::<T, 24>::new(
-                self.iterations,
-            )),
-            25 => Box::new(crate::producer::simd::SimdProducer::<T, 25>::new(
-                self.iterations,
-            )),
-            26 => Box::new(crate::producer::simd::SimdProducer::<T, 26>::new(
-                self.iterations,
-            )),
-            27 => Box::new(crate::producer::simd::SimdProducer::<T, 27>::new(
-                self.iterations,
-            )),
-            28 => Box::new(crate::producer::simd::SimdProducer::<T, 28>::new(
-                self.iterations,
-            )),
-            29 => Box::new(crate::producer::simd::SimdProducer::<T, 29>::new(
-                self.iterations,
-            )),
-            30 => Box::new(crate::producer::simd::SimdProducer::<T, 30>::new(
-                self.iterations,
-            )),
-            31 => Box::new(crate::producer::simd::SimdProducer::<T, 31>::new(
-                self.iterations,
-            )),
-            32 => Box::new(crate::producer::simd::SimdProducer::<T, 32>::new(
-                self.iterations,
-            )),
-            33 => Box::new(crate::producer::simd::SimdProducer::<T, 33>::new(
-                self.iterations,
-            )),
-            34 => Box::new(crate::producer::simd::SimdProducer::<T, 34>::new(
-                self.iterations,
-            )),
-            35 => Box::new(crate::producer::simd::SimdProducer::<T, 35>::new(
-                self.iterations,
-            )),
-            36 => Box::new(crate::producer::simd::SimdProducer::<T, 36>::new(
-                self.iterations,
-            )),
-            37 => Box::new(crate::producer::simd::SimdProducer::<T, 37>::new(
-                self.iterations,
-            )),
-            38 => Box::new(crate::producer::simd::SimdProducer::<T, 38>::new(
-                self.iterations,
-            )),
-            39 => Box::new(crate::producer::simd::SimdProducer::<T, 39>::new(
-                self.iterations,
-            )),
-            40 => Box::new(crate::producer::simd::SimdProducer::<T, 40>::new(
-                self.iterations,
-            )),
-            41 => Box::new(crate::producer::simd::SimdProducer::<T, 41>::new(
-                self.iterations,
-            )),
-            42 => Box::new(crate::producer::simd::SimdProducer::<T, 42>::new(
-                self.iterations,
-            )),
-            43 => Box::new(crate::producer::simd::SimdProducer::<T, 43>::new(
-                self.iterations,
-            )),
-            44 => Box::new(crate::producer::simd::SimdProducer::<T, 44>::new(
-                self.iterations,
-            )),
-            45 => Box::new(crate::producer::simd::SimdProducer::<T, 45>::new(
-                self.iterations,
-            )),
-            46 => Box::new(crate::producer::simd::SimdProducer::<T, 46>::new(
-                self.iterations,
-            )),
-            47 => Box::new(crate::producer::simd::SimdProducer::<T, 47>::new(
-                self.iterations,
-            )),
-            48 => Box::new(crate::producer::simd::SimdProducer::<T, 48>::new(
-                self.iterations,
-            )),
-            49 => Box::new(crate::producer::simd::SimdProducer::<T, 49>::new(
-                self.iterations,
-            )),
-            50 => Box::new(crate::producer::simd::SimdProducer::<T, 50>::new(
-                self.iterations,
-            )),
-            51 => Box::new(crate::producer::simd::SimdProducer::<T, 51>::new(
-                self.iterations,
-            )),
-            52 => Box::new(crate::producer::simd::SimdProducer::<T, 52>::new(
-                self.iterations,
-            )),
-            53 => Box::new(crate::producer::simd::SimdProducer::<T, 53>::new(
-                self.iterations,
-            )),
-            54 => Box::new(crate::producer::simd::SimdProducer::<T, 54>::new(
-                self.iterations,
-            )),
-            55 => Box::new(crate::producer::simd::SimdProducer::<T, 55>::new(
-                self.iterations,
-            )),
-            56 => Box::new(crate::producer::simd::SimdProducer::<T, 56>::new(
-                self.iterations,
-            )),
-            57 => Box::new(crate::producer::simd::SimdProducer::<T, 57>::new(
-                self.iterations,
-            )),
-            58 => Box::new(crate::producer::simd::SimdProducer::<T, 58>::new(
-                self.iterations,
-            )),
-            59 => Box::new(crate::producer::simd::SimdProducer::<T, 59>::new(
-                self.iterations,
-            )),
-            60 => Box::new(crate::producer::simd::SimdProducer::<T, 60>::new(
-                self.iterations,
-            )),
-            61 => Box::new(crate::producer::simd::SimdProducer::<T, 61>::new(
-                self.iterations,
-            )),
-            62 => Box::new(crate::producer::simd::SimdProducer::<T, 62>::new(
-                self.iterations,
-            )),
-            63 => Box::new(crate::producer::simd::SimdProducer::<T, 63>::new(
-                self.iterations,
-            )),
-            64 => Box::new(crate::producer::simd::SimdProducer::<T, 64>::new(
-                self.iterations,
-            )),
-            _ => unreachable!("clap ensures that lanes should be between 0 and 64"),
+            1 => self.create_simd_producer::<T, 1>(),
+            2 => self.create_simd_producer::<T, 2>(),
+            3 => self.create_simd_producer::<T, 3>(),
+            4 => self.create_simd_producer::<T, 4>(),
+            5 => self.create_simd_producer::<T, 5>(),
+            6 => self.create_simd_producer::<T, 6>(),
+            7 => self.create_simd_producer::<T, 7>(),
+            8 => self.create_simd_producer::<T, 8>(),
+            9 => self.create_simd_producer::<T, 9>(),
+            10 => self.create_simd_producer::<T, 10>(),
+            11 => self.create_simd_producer::<T, 11>(),
+            12 => self.create_simd_producer::<T, 12>(),
+            13 => self.create_simd_producer::<T, 13>(),
+            14 => self.create_simd_producer::<T, 14>(),
+            15 => self.create_simd_producer::<T, 15>(),
+            16 => self.create_simd_producer::<T, 16>(),
+            17 => self.create_simd_producer::<T, 17>(),
+            18 => self.create_simd_producer::<T, 18>(),
+            19 => self.create_simd_producer::<T, 19>(),
+            20 => self.create_simd_producer::<T, 20>(),
+            21 => self.create_simd_producer::<T, 21>(),
+            22 => self.create_simd_producer::<T, 22>(),
+            23 => self.create_simd_producer::<T, 23>(),
+            24 => self.create_simd_producer::<T, 24>(),
+            25 => self.create_simd_producer::<T, 25>(),
+            26 => self.create_simd_producer::<T, 26>(),
+            27 => self.create_simd_producer::<T, 27>(),
+            28 => self.create_simd_producer::<T, 28>(),
+            29 => self.create_simd_producer::<T, 29>(),
+            30 => self.create_simd_producer::<T, 30>(),
+            31 => self.create_simd_producer::<T, 31>(),
+            32 => self.create_simd_producer::<T, 32>(),
+            33 => self.create_simd_producer::<T, 33>(),
+            34 => self.create_simd_producer::<T, 34>(),
+            35 => self.create_simd_producer::<T, 35>(),
+            36 => self.create_simd_producer::<T, 36>(),
+            37 => self.create_simd_producer::<T, 37>(),
+            38 => self.create_simd_producer::<T, 38>(),
+            39 => self.create_simd_producer::<T, 39>(),
+            40 => self.create_simd_producer::<T, 40>(),
+            41 => self.create_simd_producer::<T, 41>(),
+            42 => self.create_simd_producer::<T, 42>(),
+            43 => self.create_simd_producer::<T, 43>(),
+            44 => self.create_simd_producer::<T, 44>(),
+            45 => self.create_simd_producer::<T, 45>(),
+            46 => self.create_simd_producer::<T, 46>(),
+            47 => self.create_simd_producer::<T, 47>(),
+            48 => self.create_simd_producer::<T, 48>(),
+            49 => self.create_simd_producer::<T, 49>(),
+            50 => self.create_simd_producer::<T, 50>(),
+            51 => self.create_simd_producer::<T, 51>(),
+            52 => self.create_simd_producer::<T, 52>(),
+            53 => self.create_simd_producer::<T, 53>(),
+            54 => self.create_simd_producer::<T, 54>(),
+            55 => self.create_simd_producer::<T, 55>(),
+            56 => self.create_simd_producer::<T, 56>(),
+            57 => self.create_simd_producer::<T, 57>(),
+            58 => self.create_simd_producer::<T, 58>(),
+            59 => self.create_simd_producer::<T, 59>(),
+            60 => self.create_simd_producer::<T, 60>(),
+            61 => self.create_simd_producer::<T, 61>(),
+            62 => self.create_simd_producer::<T, 62>(),
+            63 => self.create_simd_producer::<T, 63>(),
+            64 => self.create_simd_producer::<T, 64>(),
+            _ => self.create_naive_producer::<T>(),
         }
     }
 }
